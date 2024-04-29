54e25e536c192d00e8ddd1006339fbbbc165f686
batanoffs <batanoff.s@protonmail.com>
2024-04-29 22:48:53 +0300

    improved type check added favicon fixed job titiles

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,10 +1,24 @@
 import { FC } from 'react'
 
-type Props = {
-    data?: (typeof data)[]
+type ExperienceProps = {
+    data?: {
+        work: Array<{
+            id: string
+            date: string
+            company: string
+            position: Array<string>
+            description: string | Array<string>
+            technologies: string
+            website: string
+            skills: Array<{
+                id: string
+                name: string
+            }>
+        }>
+    }
 }
 
-export const Experience: FC<Props> = ({ data = [] }) => {
+export const Experience: FC<ExperienceProps> = ({ data = [] }) => {
     return (
         <section
             id="experience"
@@ -22,110 +36,106 @@ export const Experience: FC<Props> = ({ data = [] }) => {
             </div>
             <div>
                 <ol className="group/list">
-                    {data.work.map(
-                        (work: {
-                            id: string
-                            date: string
-                            company: string
-                            position: string
-                            description: string
-                            technologies: string
-                            website: string
-                        }) => (
-                            <li key={work.id} className="mb-12">
-                                <div
-                                    className="group relative grid pb-1 transition-all sm:grid-cols-8
+                    {data.work.map((work) => (
+                        <li key={work.id} className="mb-12">
+                            <div
+                                className="group relative grid pb-1 transition-all sm:grid-cols-8
                                     sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50"
-                                >
-                                    <div
-                                        className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition
+                            >
+                                <div
+                                    className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition
                                         motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50
                                         lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"
-                                    ></div>
-                                    <header
-                                        className="z-10 mb-2 mt-1 text-xs font-semibold uppercase
+                                ></div>
+                                <header
+                                    className="z-10 mb-2 mt-1 text-xs font-semibold uppercase
                                             tracking-wide text-slate-500 sm:col-span-2"
-                                    >
-                                        {work.date}
-                                    </header>
-                                    <div className="z-10 sm:col-span-6">
-                                        <h3 className="font-medium leading-snug text-slate-200">
-                                            <div>
-                                                <a
-                                                    className="inline-flex items-baseline font-medium 
+                                >
+                                    {work.date}
+                                </header>
+                                <div className="z-10 sm:col-span-6">
+                                    <h3 className="font-medium leading-snug text-slate-200">
+                                        <div>
+                                            <a
+                                                className="inline-flex items-baseline font-medium 
                                                         leading-tight text-slate-200 hover:text-teal-300 
                                                         focus-visible:text-teal-300 group/link text-base"
-                                                    href={work.website}
-                                                    target="_blank"
-                                                    rel="noreferrer noopener"
-                                                    aria-label="Senior Frontend Engineer, Accessibility at Klaviyo (opens in a new tab)"
-                                                >
-                                                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                                                    <span>
-                                                        {work.position}
-                                                        {' at '}
-                                                        <span className="inline-block">
-                                                            {work.company}
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                viewBox="0 0 20 20"
-                                                                fill="currentColor"
-                                                                className="inline-block h-4 w-4 shrink-0 transition-transform
+                                                href={work.website}
+                                                target="_blank"
+                                                rel="noreferrer noopener"
+                                                aria-label="Senior Frontend Engineer, Accessibility at Klaviyo (opens in a new tab)"
+                                            >
+                                                <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                                                <span>
+                                                    {work.position[0]}
+                                                    {' at '}
+                                                    <span className="inline-block">
+                                                        {work.company}
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 20 20"
+                                                            fill="currentColor"
+                                                            className="inline-block h-4 w-4 shrink-0 transition-transform
                                                                     group-hover/link:-translate-y-1 group-hover/link:translate-x-1
                                                                     group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1
                                                                     motion-reduce:transition-none ml-1 translate-y-px"
-                                                                aria-hidden="true"
-                                                            >
-                                                                <path
-                                                                    fill-rule="evenodd"
-                                                                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                                                                    clip-rule="evenodd"
-                                                                ></path>
-                                                            </svg>
-                                                        </span>
+                                                            aria-hidden="true"
+                                                        >
+                                                            <path
+                                                                fill-rule="evenodd"
+                                                                d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                                                clip-rule="evenodd"
+                                                            ></path>
+                                                        </svg>
                                                     </span>
-                                                </a>
-                                            </div>
-                                        </h3>
-                                        <p className="mt-2 text-sm leading-normal">
-                                            {Array.isArray(work.description) ? (
-                                                <ul>
-                                                    {work.description.map(
-                                                        (bullet: string, index: number) => (
-                                                            <li
-                                                                className="ml-4 list-disc"
-                                                                key={index}
-                                                            >
-                                                                <p>{bullet}</p>
-                                                            </li>
-                                                        )
-                                                    )}
-                                                </ul>
-                                            ) : null}
-                                        </p>
-                                        <ul
-                                            className="mt-2 flex flex-wrap"
-                                            aria-label="Technologies used"
-                                        >
-                                            {work.skills.map(
-                                                (skill: { id: string; name: string }) => (
-                                                    <li key={skill.id} className="mr-1.5 mt-2">
-                                                        <div
-                                                            className="flex items-center rounded-full
+                                                </span>
+                                            </a>
+                                        </div>
+                                        {work.position.length > 1
+                                            ? work.position
+                                                  .slice(1)
+                                                  .map((title: string, index: number) => (
+                                                      <div key={index + 1} aria-hidden="true">
+                                                          <span className="text-slate-500">
+                                                              {title}
+                                                          </span>
+                                                      </div>
+                                                  ))
+                                            : null}
+                                    </h3>
+                                    <p className="mt-2 text-sm leading-normal">
+                                        {Array.isArray(work.description) ? (
+                                            <ul>
+                                                {work.description.map(
+                                                    (bullet: string, index: number) => (
+                                                        <li className="ml-4 list-disc" key={index}>
+                                                            <p>{bullet}</p>
+                                                        </li>
+                                                    )
+                                                )}
+                                            </ul>
+                                        ) : null}
+                                    </p>
+                                    <ul
+                                        className="mt-2 flex flex-wrap"
+                                        aria-label="Technologies used"
+                                    >
+                                        {work.skills.map((skill) => (
+                                            <li key={skill.id} className="mr-1.5 mt-2">
+                                                <div
+                                                    className="flex items-center rounded-full
                                                         bg-teal-400/10 px-3 py-1 text-xs font-medium
                                                         leading-5 text-teal-300"
-                                                        >
-                                                            {skill.name}
-                                                        </div>
-                                                    </li>
-                                                )
-                                            )}
-                                        </ul>
-                                    </div>
+                                                >
+                                                    {skill.name}
+                                                </div>
+                                            </li>
+                                        ))}
+                                    </ul>
                                 </div>
-                            </li>
-                        )
-                    )}
+                            </div>
+                        </li>
+                    ))}
                 </ol>
             </div>
         </section>
