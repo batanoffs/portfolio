aa649293bf03ab6ed945e596c9e1e129a07fd33d
batanoffs <batanoff.s@protonmail.com>
2024-10-13 17:36:56 +0300

    add mainContent type interface and import it

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,52 +2,7 @@ import { Experience } from './Experience';
 import { Projects } from './Projects';
 import { About } from './About';
 import { Certificates } from './Certificates';
-
-interface MainContentProps {
-    data: {
-        work: Array<{
-            id: string;
-            date: string;
-            position: string[];
-            company: string;
-            website: string;
-            description: string[];
-            skills: Array<{
-                id: string;
-                name: string;
-            }>;
-        }>;
-        projects: Array<{
-            id: string;
-            label: string;
-            title: string;
-            description: string;
-            image: string;
-            href: string;
-            readme: string;
-            video: string;
-            links: {
-                live?: string;
-                github?: string;
-                npm?: string;
-            };
-            badges: {
-                type: string;
-                link: string;
-            }[];
-            tags: {
-                id: string;
-                name: string;
-            }[];
-        }>;
-        certificates: Array<{
-            name: string;
-            date: string;
-            img: string;
-            link: string;
-        }>;
-    };
-}
+import { MainContentProps } from '../interfaces/main.interface';
 
 export const MainContent = ({ data }: MainContentProps) => {
     return (
