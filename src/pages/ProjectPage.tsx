6c4d94dc5444faf749c9060d7f41ff752f46c838
batanoffs <batanoff.s@protonmail.com>
2024-11-13 15:17:27 +0200

    import constants and update component

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -7,31 +7,20 @@ import remarkGfm from 'remark-gfm';
 import { CodeSnippet } from '../utils/CodeSnippetRender';
 import { ProjectLinks } from '../components/ProjectLinks';
 import { ProjectProps } from '../interfaces/project.interface';
+import { RA_AUTH_TITLE, MINI_FINANCE_URL, KNIGI_BG_TITLE } from '../constants/common';
 
 export const ProjectPage = ({ project, styles }: ProjectProps) => {
     const { data, loading, error } = useFetchReadme(project.readme);
-
-    const raAuthTitle = 'ra-auth-ui';
-
-    const miniFinance = data.includes(
-        'https://github.com/batanoffs/mini-finance/assets/111650687/d3e5ef51-4f2d-4225-a05e-6da467265a31'
-    );
-
     let newData;
+
+    const miniFinance = data.includes(MINI_FINANCE_URL);
+
     if (miniFinance) {
-        newData = data.replace(
-            /https:\/\/github.com\/batanoffs\/mini-finance\/assets\/111650687\/d3e5ef51-4f2d-4225-a05e-6da467265a31/g,
-            ''
-        );
+        newData = data.replace(/https:\/\/github.com\/batanoffs\/mini-finance\/assets\/111650687\/d3e5ef51-4f2d-4225-a05e-6da467265a31/g, '');
     }
 
-    if (loading) {
-        return <div className="text-center text-2xl my-4 text-slate-400">Loading...</div>;
-    }
-
-    if (error) {
-        return <div className="text-red-500">Error: {error.message}</div>;
-    }
+    if (loading) return <div className="text-center text-2xl my-4 text-slate-400">Loading...</div>;
+    if (error) return <div className="text-red-500">Error fetching data: {error.message}</div>;
 
     return (
         <div className={styles}>
@@ -40,10 +29,7 @@ export const ProjectPage = ({ project, styles }: ProjectProps) => {
                 <ProjectLinks links={project.links} />
             </div>
             {miniFinance && (
-                <section
-                    aria-label="Video"
-                    className="relative aspect-video mb-4 scroll-mt-16 md:mb-0 lg:mb-8 lg:scroll-mt-12"
-                >
+                <section aria-label="Video" className="relative aspect-video mb-4 scroll-mt-16 md:mb-0 lg:mb-8 lg:scroll-mt-12">
                     <ReactPlayer
                         className="absolute top-0 left-0"
                         width="100%"
@@ -66,25 +52,19 @@ export const ProjectPage = ({ project, styles }: ProjectProps) => {
                         <h1
                             id={children?.toString().toLowerCase().replace(/\s+/g, '-')}
                             className={`text-3xl ${miniFinance ? 'hidden' : ''} ${
-                                children?.toString().includes(raAuthTitle) ? 'hidden' : ''
+                                children?.toString().includes(RA_AUTH_TITLE || KNIGI_BG_TITLE) ? 'hidden' : ''
                             } font-bold my-4`}
                         >
                             {children}
                         </h1>
                     ),
                     h2: ({ children }) => (
-                        <h2
-                            id={children?.toString().toLowerCase().replace(/\s+/g, '-')}
-                            className="text-2xl font-bold my-4"
-                        >
+                        <h2 id={children?.toString().toLowerCase().replace(/\s+/g, '-')} className="text-2xl font-bold my-4">
                             {children}
                         </h2>
                     ),
                     h3: ({ children }) => (
-                        <h3
-                            id={children?.toString().toLowerCase().replace(/\s+/g, '-')}
-                            className="text-xl font-bold my-4"
-                        >
+                        <h3 id={children?.toString().toLowerCase().replace(/\s+/g, '-')} className="text-xl font-bold my-4">
                             {children}
                         </h3>
                     ),
@@ -97,6 +77,11 @@ export const ProjectPage = ({ project, styles }: ProjectProps) => {
                         </a>
                     ),
                     li: ({ children }) => <li className="ml-4 list-disc">{children}</li>,
+                    table: ({ children }) => (
+                        <div className="overflow-x-auto">
+                            <table className="w-full border border-slate-700">{children}</table>
+                        </div>
+                    ),
                     code: (props) => {
                         const { children, className, ...rest } = props;
                         const match = /language-(\w+)/.exec(className || '');
