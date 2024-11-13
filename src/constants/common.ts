96229088416959b51060a20ddf74fe86b3b4f65a
batanoffs <batanoff.s@protonmail.com>
2024-11-13 15:15:37 +0200

    add constants

---
+++ src/constants/common.ts
@@ -0,0 +1,3 @@
+export const RA_AUTH_TITLE = 'ra-auth-ui';
+export const MINI_FINANCE_URL = 'https://github.com/batanoffs/mini-finance/assets/111650687/d3e5ef51-4f2d-4225-a05e-6da467265a31';
+export const KNIGI_BG_TITLE = 'E-commerce Bookstore "Knigi.bg"';
