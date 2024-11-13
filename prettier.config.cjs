6d5225aaad9ecf709bc6d0990ddf26335e27f4a6
batanoffs <batanoff.s@protonmail.com>
2024-11-13 15:15:21 +0200

    update config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     tabWidth: 4,
     semi: true,
     singleQuote: true,
-    printWidth: 100,
+    printWidth: 150,
     bracketSpacing: true,
     jsxBracketSameLine: false,
     quoteProps: 'as-needed',
