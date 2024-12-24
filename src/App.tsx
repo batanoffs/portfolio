446ba80e2b9a1b6e4aafe4ebbe09b62671ee1a52
batanoffs <batanoff.s@protonmail.com>
2024-12-24 17:02:03 +0200

    update imports and add comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,67 @@
-import { Routes, Route } from 'react-router-dom';
+/**
+ * App component:
+ * The root component of the application, which renders
+ * the main layout of the application and their children.
+ *
+ * @component App.tsx
+ * @returns {JSX.Element} A JSX element representing the App component.
+ */
 
-import { MainContent } from './components/MainContent';
-import { Footer } from './components/Footer';
-import { MasterLayout } from './layout/MasterLayout';
-import { MainLayout } from './layout/MainLayout';
-import { SocialIcons } from './components/SocialIcons';
-import { JobTitleAnimatedText } from './components/JobTitle';
-import { Navigation } from './components/Navigation';
-import { SideLayout } from './layout/SideLayout';
-import { ProjectPage } from './pages/ProjectPage';
-import { ScrollToTop } from './utils/ScrollToTop';
-import { ScrollToAnchor } from './utils/ScrollToAnchor';
-import { SkillButtons } from './components/SkillButtons';
+import { Routes, Route } from 'react-router-dom';
+import { JobTitle, MainContent, Footer, SocialIcons, SkillButtons, Navigation } from './components/index';
+import { MasterLayout, MainLayout, SideLayout } from './components/layout/index';
+import { ScrollToAnchor, ScrollToTop } from './utils/index';
+import { ProjectPage } from './components/project-page/ProjectPage';
 
 import data from './data.json';
-
 import './index.css';
 
 export const App = () => {
-    ScrollToAnchor();
 
+    //Render elements
     return (
+
+        // Render the master layout which wraps the application
         <MasterLayout>
+
+             {/* Init the scroll to anchor component  */}
+            <ScrollToAnchor />
+
+            {/* Render the side layout */}
             <SideLayout>
-                <JobTitleAnimatedText />
-                <SkillButtons styles="flex flex-col lg:flex-row md:flex-row gap-2 overflow-hidden h-[auto] mb-6" />
+                <JobTitle />
+                <SkillButtons />
                 <Navigation />
                 <SocialIcons />
             </SideLayout>
+
+            {/* Render the main layout */}
             <MainLayout>
+
+                {/* Define routes */}
                 <Routes>
+
+                    {/* Main content route */}
                     <Route path="/" element={<MainContent data={data} />} />
+
+                    {/* Map through the projects and render routes for each one */}
                     {data.projects.map((project) => (
                         <Route
                             key={project.id}
                             path={project.href}
-                            element={
-                                <ProjectPage
-                                    styles="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
-                                    project={project}
-                                />
-                            }
+                            element={<ProjectPage {...project} />}
                         />
                     ))}
                 </Routes>
+
+                {/* Render the footer */}
                 <Footer />
+
+                {/* Init the scroll to top component  */}
                 <ScrollToTop />
+
             </MainLayout>
+            
         </MasterLayout>
     );
 };
