4df8f25f10df2f098f497c3743f138ef3b7509cd
batanoffs <batanoff.s@protonmail.com>
2024-12-24 16:59:12 +0200

    add component to render each skill, add style to hold styles as constant

---
+++ src/components/skill-buttons/styles.ts
@@ -0,0 +1,4 @@
+export const animationButton = `hover:animate-pulse px-2 py-0.5 
+    border-2 text-sm shadow rounded-2xl bg-slate-200/10 
+    hover:text-teal-400 hover:border-teal-600 border-slate-800 cursor-pointer
+    hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] hover:drop-shadow-lg`;
