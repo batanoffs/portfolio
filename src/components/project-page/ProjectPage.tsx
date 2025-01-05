dd6b4727c7ce5697d91a6de570faf6aa2ccaec7b
batanoffs <batanoff.s@protonmail.com>
2025-01-05 19:36:39 +0200

    improve logic for rendering items

--- src/components/project-page/ProjectPage.tsx
+++ src/components/project-page/ProjectPage.tsx
@@ -1,10 +1,12 @@
 /**
  * Project Page
- * A component that renders a project page based on the project metadata input.
+ *
+ * A component that fetches the readme file of the project from github and
+ * renders the content with the help of Markdown and remarkGfm libraries.
  *
  * @component ProjectPage.tsx
- * @param {Project} project - The project metadata.
- * @returns {JSX.Element} The rendered project page.
+ * @param {ProjectProps} project - The project metadata.
+ * @returns {JSX.Element} The parsed readme for the current project.
  */
 
 import { ReactNode } from 'react';
@@ -13,43 +15,33 @@ import remarkGfm from 'remark-gfm';
 
 import { useFetchReadme } from './useFetchReadme';
 import { ProjectLinks } from './ProjectLinks';
-import { CodeSnippet } from '../../utils/index';
 import { ProjectProps } from './projects/project.interface';
 import { MINI_FINANCE_URL } from '../../constants/common';
-// import ReactPlayer from 'react-player/file';
+import { CodeSnippet, Loading } from '../../utils';
+import ReactPlayer from 'react-player';
 
 export const ProjectPage = (project: ProjectProps) => {
-    
     // Call hook to obtain data
     const { data, loading, error } = useFetchReadme(project.readme);
 
-    // Declare variable
-    let newData;
-
-    // Save string to var if incudes
-    const miniFinance = data.includes(MINI_FINANCE_URL);
-
-    // Check if true
-    if (miniFinance) {
-        
-        // Replace the data and save it to newData
-        newData = data.replace(
-            /https:\/\/github.com\/batanoffs\/mini-finance\/assets\/111650687\/d3e5ef51-4f2d-4225-a05e-6da467265a31/g,
-            ''
-        );
-    }
-
     // Check if the content is loading and render loading state
-    if (loading) return <div className="text-center text-2xl my-4 text-slate-400">Loading...</div>;
+    if (loading) return <Loading />;
 
     // Check if error occurred and render error
-    if (error) return <div className="text-red-500">Error fetching data: {error.message}</div>;
+    if (error) {
+        return (
+            <div className="text-red-500">
+                Unexpected error occurred while fetching data. Please reload the page.
+            </div>
+        );
+    }
 
     // Return component JSX logic and elements
     return (
         <div className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
             <Markdown
-                children={miniFinance ? newData : data}
+                // children={miniFinance ? miniFinanceDataContext : data}
+                children={data}
                 skipHtml={false}
                 remarkPlugins={[remarkGfm]}
                 components={{
@@ -62,22 +54,6 @@ export const ProjectPage = (project: ProjectProps) => {
                                 {children}
                             </h1>
                             <ProjectLinks {...project.links} />
-                            {/* <div>
-                                {miniFinance && (
-                                    <section aria-label="Video" className="relative aspect-video mb-4 scroll-mt-16 md:mb-0 lg:mb-8 lg:scroll-mt-12">
-                                        <ReactPlayer
-                                            className="absolute top-0 left-0"
-                                            width="100%"
-                                            height="100%"
-                                            url={project.video}
-                                            muted={true}
-                                            playing={true}
-                                            controls={true}
-                                            pipe={'false'}
-                                        />
-                                    </section>
-                                )}
-                            </div> */}
                         </div>
                     ),
                     h2: ({ children }) => (
@@ -113,11 +89,28 @@ export const ProjectPage = (project: ProjectProps) => {
                             </p>
                         );
                     },
-                    a: ({ href, children }) => (
-                        <a href={href} className="text-blue-500 underline hover:text-blue-700">
-                            {children}
-                        </a>
-                    ),
+                    a: ({ href, children }) =>
+                        href && href === MINI_FINANCE_URL ? (
+                            <section
+                                aria-label="Video"
+                                className="relative aspect-video mb-4 scroll-mt-16 md:mb-0 lg:mb-8 lg:scroll-mt-12"
+                            >
+                                <ReactPlayer
+                                    className="absolute top-0 left-0"
+                                    width="100%"
+                                    height="100%"
+                                    url={href}
+                                    muted={true}
+                                    playing={true}
+                                    controls={true}
+                                    pipe={false}
+                                />
+                            </section>
+                        ) : (
+                            <a href={href} className="text-blue-500 underline hover:text-blue-700">
+                                {children}
+                            </a>
+                        ),
 
                     ul: ({ children }) => <ul className="ml-4 list-disc">{children}</ul>,
                     table: ({ children }) => (
