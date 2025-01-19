2076068fc1039c0f7be191dab0c57fc7b439fd84
batanoffs <batanoff.s@protonmail.com>
2025-01-19 16:52:40 +0200

    update and rename constants

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,3 @@
 export const RA_AUTH_TITLE = 'ra-auth-ui';
-export const MINI_FINANCE_URL = 'https://github.com/batanoffs/mini-finance/assets/111650687/d3e5ef51-4f2d-4225-a05e-6da467265a31';
-export const KNIGI_BG_TITLE = 'E-commerce Bookstore "Knigi.bg"';
+export const MINI_FINANCE_DEMO_URL = 'https://github.com/batanoffs/mini-finance/assets/111650687/d3e5ef51-4f2d-4225-a05e-6da467265a31';
+export const KNIGI_BG_DEMO_URL = 'https://www.youtube.com/watch?v=0UT5XTyFcRU';
