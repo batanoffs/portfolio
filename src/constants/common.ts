6119b4992584ab3177570fe03b726ce5c814bdcc
batanoffs <batanoff.s@protonmail.com>
2025-01-19 17:08:25 +0200

    refactor demo URLs into a single object for better organization

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,8 @@
 export const RA_AUTH_TITLE = 'ra-auth-ui';
-export const MINI_FINANCE_DEMO_URL = 'https://github.com/batanoffs/mini-finance/assets/111650687/d3e5ef51-4f2d-4225-a05e-6da467265a31';
-export const KNIGI_BG_DEMO_URL = 'https://www.youtube.com/watch?v=0UT5XTyFcRU';
+
+export const DEMO_URLS = {
+    miniFinance:
+        'https://github.com/batanoffs/mini-finance/assets/111650687/d3e5ef51-4f2d-4225-a05e-6da467265a31',
+    knigiBg: 'https://www.youtube.com/watch?v=0UT5XTyFcRU',
+    aiBuilder: 'https://github.com/user-attachments/assets/39f5e75c-cdb0-47eb-8e98-1da768a64e3b',
+};
