bfcbdd49ff8a0f824b567615b52c448fbda2bee1
batanoffs <batanoff.s@protonmail.com>
2025-01-29 07:35:07 +0200

    moved

---
+++ src/components/project-page/projects/types/project.interface.ts
@@ -0,0 +1,29 @@
+export interface ProjectProps {
+    id: string;
+    label: string;
+    title: string;
+    description: string;
+    bullets?: string[];
+    image: string;
+    href: string;
+    readme: string;
+    video: string;
+    links: {
+        live?: string;
+        github?: string;
+        npm?: string;
+    };
+    badges: {
+        type: string;
+        link: string;
+    }[];
+    tags: {
+        id: string;
+        name: string;
+    }[];
+}
+
+export interface ProjectListProps {
+    projects?: ProjectProps[];
+    styles?: string;
+}
